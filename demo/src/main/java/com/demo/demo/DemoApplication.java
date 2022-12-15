1b8ce71cbe00678b2375d945cad7f65533bd335d
Neema <patelneema93@gmail.com>
2022-12-15 11:58:37 -0500

    create Mario game class

--- demo/src/main/java/com/demo/demo/DemoApplication.java
+++ demo/src/main/java/com/demo/demo/DemoApplication.java
@@ -3,11 +3,18 @@ package com.demo.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.demo.demo.game.GameRunner;
+import com.demo.demo.game.MarioGame;
+
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		MarioGame game = new MarioGame();
+		GameRunner runner = new GameRunner(game);
+		runner.run();
+
 	}
 
 }
