17deb549251772dcf87c0cce6d527cde36e6e2c9
Neema <patelneema93@gmail.com>
2022-12-15 12:58:10 -0500

    create pacmanGame class

--- demo/src/main/java/com/demo/demo/DemoApplication.java
+++ demo/src/main/java/com/demo/demo/DemoApplication.java
@@ -4,16 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.demo.demo.game.GameRunner;
-import com.demo.demo.game.MarioGame;
+//import com.demo.demo.game.MarioGame;
 //import com.demo.demo.game.SuperContraGame;
+import com.demo.demo.game.PacmanGame;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		MarioGame game = new MarioGame();
+		//MarioGame game = new MarioGame();
 		//SuperContraGame game = new SuperContraGame();
+		PacmanGame game = new PacmanGame();
 		GameRunner runner = new GameRunner(game);
 		runner.run();
 
