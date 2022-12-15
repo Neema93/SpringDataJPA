95be8b4d859beed26d34ce1eeaada2d7f9dce90b
Neema <patelneema93@gmail.com>
2022-12-15 11:47:37 -0500

    Game Runner Project

--- demo/src/main/java/com/demo/demo/repository/CourseSpringDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.demo.demo.entity.Course;
-
-public interface CourseSpringDataRepository extends JpaRepository<Course, Long>{
-    
-}
