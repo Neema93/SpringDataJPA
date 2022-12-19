8bb00cc43130777c306761c00a6165a0da959172
Neema <patelneema93@gmail.com>
2022-12-19 11:08:53 -0500

    create User Class

---
+++ demo/src/main/java/com/demo/demo/socialmediaapp/entity/User.java
@@ -0,0 +1,42 @@
+package com.demo.demo.socialmediaapp.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+@Entity
+public class User {
+    private Integer id;
+    private String name;
+    private LocalDate birthDate;
+    public User(Integer id, String name, LocalDate birthDate) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
+    }
+    
+    
+    
+    
+}
