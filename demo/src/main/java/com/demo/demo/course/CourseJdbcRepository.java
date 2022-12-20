4bd0a8f751b4fe0f7dde56041b82b752ba6c151e
Neema <patelneema93@gmail.com>
2022-12-20 10:32:58 -0500

    connect to command line runner

--- demo/src/main/java/com/demo/demo/course/CourseJdbcRepository.java
+++ demo/src/main/java/com/demo/demo/course/CourseJdbcRepository.java
@@ -1,9 +1,11 @@
 package com.demo.demo.course;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 @Repository
 public class CourseJdbcRepository {
+    @Autowired
    private JdbcTemplate springJdbcTemplate;
 
    private static String INSERT_QUERY = """
@@ -12,4 +14,5 @@ public class CourseJdbcRepository {
    public void insert(){
     springJdbcTemplate.update(INSERT_QUERY);
    }
+
 }
