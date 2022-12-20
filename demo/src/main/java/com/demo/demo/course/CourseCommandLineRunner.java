0024db804876d92a033ea5053fab6c841fb3bdcd
Neema <patelneema93@gmail.com>
2022-12-20 11:18:32 -0500

    create JPA Repository

--- demo/src/main/java/com/demo/demo/course/CourseCommandLineRunner.java
+++ demo/src/main/java/com/demo/demo/course/CourseCommandLineRunner.java
@@ -4,16 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 @Component
-public class CourseJdbcCommandLineRunner  implements CommandLineRunner{
-   @Autowired
-    private CourseJdbcRepository repository;
+public class CourseCommandLineRunner  implements CommandLineRunner{
+//    @Autowired
+//     private CourseJdbcRepository repository;
+    @Autowired
+    private CourseJpaRepository repository;
     @Override
     public void run(String... args) throws Exception {
 
         repository.insert(new Course(1L, "Learn Java Basic" ,"Neema"));
         repository.insert(new Course(2L, "Learn Java Advance" ,"Neema"));
         repository.insert(new Course(3L, "Learn Spring boot" ,"Neema"));
-        repository.delete(3L);
+        repository.deleteById(3L);
         System.out.println(repository.findById(2L));
     }
     
---
+++ demo/src/main/java/com/demo/demo/course/CourseJpaRepository.java
@@ -0,0 +1,24 @@
+package com.demo.demo.course;
+import org.springframework.stereotype.Repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Repository
+@Transactional
+public class CourseJpaRepository {
+    @PersistenceContext
+    private EntityManager em;
+    public void insert(Course course){
+        em.merge(course);
+    }
+    public Course findById(long id){
+       return em.find(Course.class, id);
+    }
+    public void deleteById(long id){
+        Course course = em.find(Course.class, id);
+        em.remove(course);
+     }
+
+}
