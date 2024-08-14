9c6c152c230b6977b0f39285caf481ad470085de
Neema <patelneema93@gmail.com>
2024-08-14 15:32:55 -0400

    create mockitoMock test run with MockitoExtension

--- demo/src/test/java/com/demo/demo/mockito/SomeBusinessImplMockTest.java
+++ demo/src/test/java/com/demo/demo/mockito/SomeBusinessImplMockTest.java
@@ -1,23 +1,30 @@
 package com.demo.demo.mockito;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
+
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+@ExtendWith(MockitoExtension.class)
 public class SomeBusinessImplMockTest {
-  
+@Mock
+private DataService dataServiceMock;
+@InjectMocks
+private SomeBusinessImpl businessImpl;
     @Test
-    void test(){
-        DataService dataServiceMock=mock(DataService.class);
-       
-
-  when(dataServiceMock.retriveAllData()).thenReturn(new int[]{25, 15, 5});
-  
-        SomeBusinessImpl businessImpl= new SomeBusinessImpl(dataServiceMock);
-        int result=businessImpl.findTheGreatestFromAllData();
-        assertEquals(25, result);
+    void basicScenario() {
+        when(dataServiceMock.retriveAllData()).thenReturn(new int[] { 25,15, 5 });
+        assertEquals(25, businessImpl.findTheGreatestFromAllData());
     }
+    @Test
+    void oneValue() {
+        when(dataServiceMock.retriveAllData()).thenReturn(new int[] { 35});
+        assertEquals(35, businessImpl.findTheGreatestFromAllData());
+    }
+
 }
 
