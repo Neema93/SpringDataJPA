ea616115e1762dae9b1c78b7fdef884dbd298306
Neema <patelneema93@gmail.com>
2024-08-19 10:57:38 -0400

    create welcome  page

--- demo/src/main/java/com/demo/demo/indecision/loginController.java
+++ demo/src/main/java/com/demo/demo/indecision/loginController.java
@@ -1,17 +1,30 @@
 package com.demo.demo.indecision;
 
 import org.springframework.web.bind.annotation.RestController;
+
+
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 // import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class loginController {
     @RequestMapping("/login")
-    public String getMethodName() {
+    public String gotoLoginPage() {
         return "indecision/login";
     }
+    @RequestMapping(value="/welcome")
+    @PostMapping
+    public String gotoWelcomePage(@RequestParam String name,Model model) {
+        model.addAttribute("name",name);
+        return "indecision/welcome";
+    }
+
     
 }
