177a394448ae2cfa25f7dc6eb33edc5e88944021
Neema <patelneema93@gmail.com>
2024-08-19 11:11:44 -0400

    indecision entity

--- demo/src/main/java/com/demo/demo/indecision/loginController.java
+++ demo/src/main/java/com/demo/demo/indecision/loginController.java
@@ -1,18 +1,10 @@
 package com.demo.demo.indecision;
-
-import org.springframework.web.bind.annotation.RestController;
-
-
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-
 @Controller
 public class loginController {
     @RequestMapping("/login")
